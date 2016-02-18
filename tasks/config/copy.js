5c557fbebeb93a48cd877bcd850b22096a3c251b
craigvantonder <craig@compulutions.co.za>
2016-02-19 01:24:42 +0200

    3 - Edit tasks/config/copy.js

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -11,28 +11,93 @@
  * Copies all directories nd files from the .tmp/public directory into a www directory.
  *
  * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-copy
+ *    https://github.com/gruntjs/grunt-contrib-copy
  */
+
+module.exports = function(grunt) {
+  grunt.file.mkdir('./assets/fonts', 0755);
+};
+
 module.exports = function(grunt) {
 
-	grunt.config.set('copy', {
-		dev: {
-			files: [{
-				expand: true,
-				cwd: './assets',
-				src: ['**/*.!(coffee|less)'],
-				dest: '.tmp/public'
-			}]
-		},
-		build: {
-			files: [{
-				expand: true,
-				cwd: '.tmp/public',
-				src: ['**/*'],
-				dest: 'www'
-			}]
-		}
-	});
+  grunt.config.set('copy', {
+    dev: {
+      files: [{
+        expand: true,
+        cwd: './node_modules',
+        src: [
+        'jquery/dist/jquery.js',
+        'bootstrap/dist/js/bootstrap.js'
+        ],
+        flatten: true,
+        dest: 'assets/js/dependencies'
+      },{
+        expand: true,
+        cwd: './node_modules',
+        src: [
+        'bootstrap/dist/css/bootstrap.css',
+        'bootstrap/dist/css/bootstrap.css.map'
+        ],
+        flatten: true,
+        dest: 'assets/styles'
+      },{
+        expand: true,
+        cwd: './node_modules',
+        src: [
+        'bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
+        'bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
+        'bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
+        'bootstrap/dist/fonts/glyphicons-halflings-regular.woff',
+        'bootstrap/dist/fonts/glyphicons-halflings-regular.woff2'
+        ],
+        flatten: true,
+        dest: 'assets/fonts'
+      },{
+        expand: true,
+        cwd: './assets',
+        src: ['**/*.!(coffee|less)'],
+        dest: '.tmp/public'
+      }]
+    },
+    build: {
+     files: [{
+      expand: true,
+      cwd: './node_modules',
+      src: [
+      'jquery/dist/jquery.js',
+      'bootstrap/dist/js/bootstrap.js'
+      ],
+      flatten: true,
+      dest: 'assets/js/dependencies'
+    },{
+      expand: true,
+      cwd: './node_modules',
+      src: [
+      'bootstrap/dist/css/bootstrap.css',
+      'bootstrap/dist/css/bootstrap.css.map'
+      ],
+      flatten: true,
+      dest: 'assets/styles'
+    },{
+      expand: true,
+      cwd: './node_modules',
+      src: [
+      'bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
+      'bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
+      'bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
+      'bootstrap/dist/fonts/glyphicons-halflings-regular.woff',
+      'bootstrap/dist/fonts/glyphicons-halflings-regular.woff2'
+      ],
+      flatten: true,
+      dest: 'assets/fonts'
+    },{
+      expand: true,
+      cwd: '.tmp/public',
+      src: ['**/*'],
+      dest: 'www'
+    }]
+  }
+});
 
-	grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-copy');
 };
