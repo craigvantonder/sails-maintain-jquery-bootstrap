c42324244765e9d599344edd4ec68d7204596196
craigvantonder <craig@compulutions.co.za>
2016-02-19 01:28:51 +0200

    4 - Edit tasks/pipeline.js

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -27,6 +27,9 @@ var jsFilesToInject = [
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
+  // Lod jQuery first
+  'js/dependencies/**/jquery.js',
+
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
